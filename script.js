fab2723b3aa2b1dee3fa349316f7acfad03eea02
pbsora <128522273+pbsora@users.noreply.github.com>
2023-04-10 10:40:30 -0300

    script ready

--- script.js
+++ script.js
@@ -11,7 +11,41 @@ function getComputerChoice(){
     } else {
         pc = 'Rock'
     }
-    console.log(pc)
+    return pc
 }
 
-getComputerChoice()
+
+function playRound(computerSelection, playerSelection){
+
+    if(computerSelection == playerSelection){
+        console.log("Oof, that was a tie")
+    }
+    
+    else if(playerSelection == "Rock" && computerSelection == "Scissors"){
+        console.log(`You won, the computer used ${computerSelection}`)
+    }else if(playerSelection == "Scissors" && computerSelection == "Rock"){
+        console.log(`Too bad, the computer used ${computerSelection}`)
+    }
+
+    else if(playerSelection == "Scissors" && computerSelection == "Paper"){
+        console.log(`You won, the computer used ${computerSelection}`)
+    }else if(playerSelection == "Paper" && computerSelection == "Scissors"){
+        console.log(`Too bad, the computer used ${computerSelection}`)
+    }
+
+    else if(playerSelection == "Paper" && computerSelection == "Rock"){
+        console.log(`You won, the computer used ${computerSelection}`)
+    }else if(playerSelection == "Rock" && computerSelection == "Paper"){
+        console.log(`Too bad, the computer used ${computerSelection}`)
+    }
+
+}
+
+
+
+for (let i = 0; i < 5; i++){
+    let computerSelection = getComputerChoice()
+    let player = prompt("What will you play")
+    let playerSelection = player.charAt(0).toUpperCase() + player.slice(1)
+    playRound(computerSelection, playerSelection)
+}
